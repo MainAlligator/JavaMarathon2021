096d131c2d60fc4a36766236ef57b6dcf2dbbc5c
Alexi Krol <krol.mats@gmail.com>
2021-01-23 15:22:27 +0200

    Done tasks of   3 Day

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,25 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите два числа:");
+
+        double firstNumber = scanner.nextDouble();
+        double secondNumber = scanner.nextDouble();
+        System.out.println(division(firstNumber,secondNumber));
+
+    }
+
+    public  static double division (double a, double b){
+        if (b==0){
+            return -1;
+        }
+        return a/b;
 
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,23 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Введите делимое: ");
+            double firstNumber= scan.nextDouble();
+            System.out.print("Введите делитель: ");
+            double secondNumber = scan.nextDouble();
+
+            if (secondNumber == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            double result = firstNumber/secondNumber;
+            System.out.println("Результат деления: " + result);
+        }
 
     }
 }
