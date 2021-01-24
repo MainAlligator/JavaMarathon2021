b911672647ec30c7c32c96a7e643a734a4f94a83
Alexi Krol <krol.mats@gmail.com>
2021-01-24 21:02:46 +0200

    Done tasks of   5 Day

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -3,5 +3,14 @@ package day5;
 public class Task1 {
     public static void main(String[] args) {
 
+        Car carHundai = new Car();
+        carHundai.setYearOfIssue(2020);
+        carHundai.setColor("White");
+        carHundai.setModel("Elantra");
+
+        System.out.println(carHundai.getYearOfIssue());
+        System.out.println(carHundai.getColor());
+        System.out.println(carHundai.getModel());
+
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -4,5 +4,11 @@ package day5;
 public class Task2 {
     public static void main(String[] args) {
 
+        Motorbike yamaha = new Motorbike(2020,"Black", "WQ");
+
+        System.out.println(yamaha.getYearOfIssue());
+        System.out.println(yamaha.getColor());
+        System.out.println(yamaha.getModel());
+
     }
 }
