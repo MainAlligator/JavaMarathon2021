a5d5e86e55efd97988b3c1f1845dbbba0125577f
Alexi Krol <krol.mats@gmail.com>
2021-01-27 17:23:04 +0200

    Done tasks of   8 Day

---
+++ src/main/java/day8/Airplane.java
@@ -0,0 +1,73 @@
+package day8;
+
+public class Airplane {
+    private String model;
+    private String producer;
+    private int year;
+    private int length;
+    private int weight;
+    private double fuel = 0;
+
+    public Airplane(String producer, int year, int length, int weight, String model) {
+        this.producer = producer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+        this.model = model;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public void setModel(String model) {
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    @Override
+    public String toString() {
+        return "Airplane{" +
+                "model='" + model + '\'' +
+                ", producer='" + producer + '\'' +
+                ", year=" + year +
+                ", length=" + length +
+                ", weight=" + weight +
+                ", fuel=" + fuel +
+                '}';
+    }
+
+    public void fillUp(int n) {
+        fuel += n;
+    }
+
+    public static void compareAirplanes(Airplane airplane1, Airplane airplane2) {
+
+        if (airplane1.length < airplane2.length) {
+            System.out.println("Длинее самолет: " + airplane2.model);
+        } else if (airplane1.length == airplane2.length) {
+            System.out.println("Они равны. ");
+        } else {
+            System.out.println("Длинее самолет: " + airplane1.model);
+        }
+
+    }
+}
--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,39 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long start;
+        long finish;
+        long timeConsumedMillis = 0;
+        long start1;
+        long finish1;
+        long timeConsumedMillis1 = 0;
 
+
+        for (int i = 0; i <= 20000; i++) {
+
+            start = System.nanoTime();
+
+            System.out.println(i + " ");
+            finish = System.nanoTime();
+            timeConsumedMillis = finish - start;
+        }
+
+        for (int j = 0; j < 20000; j++) {
+
+
+            StringBuilder sb = new StringBuilder();
+            start1 = System.nanoTime();
+            sb.append(j);
+
+            sb.append(" ");
+            System.out.println(sb.toString());
+            finish1 = System.nanoTime();
+            timeConsumedMillis1 = finish1 - start1;
+        }
+
+        System.out.println("Время первого метода: " + timeConsumedMillis);
+        System.out.println("Время второго метода: " + timeConsumedMillis1);
     }
+
 }
+
