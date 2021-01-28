879ab63be0d9da447766db0de55901c2f8bd9706
Alexi Krol <krol.mats@gmail.com>
2021-01-28 12:11:26 +0200

    Update correct times for loops.

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -4,33 +4,34 @@ public class Task1 {
     public static void main(String[] args) {
         long start;
         long finish;
-        long timeConsumedMillis = 0;
+        long timeConsumedMillis;
         long start1;
         long finish1;
-        long timeConsumedMillis1 = 0;
+        long timeConsumedMillis1;
+        String string  =" ";
 
 
+        start = System.nanoTime();
         for (int i = 0; i <= 20000; i++) {
+            string  = i+ " ";
 
-            start = System.nanoTime();
-
-            System.out.println(i + " ");
-            finish = System.nanoTime();
-            timeConsumedMillis = finish - start;
         }
+        finish = System.nanoTime();
+        timeConsumedMillis = finish - start;
 
+        System.out.println(string);
+
+        start1 = System.nanoTime();
+        StringBuilder sb = new StringBuilder();
         for (int j = 0; j < 20000; j++) {
-
-
-            StringBuilder sb = new StringBuilder();
-            start1 = System.nanoTime();
             sb.append(j);
 
             sb.append(" ");
-            System.out.println(sb.toString());
-            finish1 = System.nanoTime();
-            timeConsumedMillis1 = finish1 - start1;
         }
+        finish1 = System.nanoTime();
+        timeConsumedMillis1 = finish1 - start1;
+
+        System.out.println(sb.toString());
 
         System.out.println("Время первого метода: " + timeConsumedMillis);
         System.out.println("Время второго метода: " + timeConsumedMillis1);
